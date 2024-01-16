df4275c8a19aa0bc6c43daf98e29639220627d84
Manish Gautam <manish.gautam.india@gmail.com>
2024-01-16 13:34:38 +0530

    Updates

--- App.tsx
+++ App.tsx
@@ -1,42 +1,52 @@
-import React from 'react';
-import useEffectOnce from '@/shared/hooks/useEffectOnce';
-import { storage } from '@/shared/helpers';
-import i18n from 'i18next';
+import React, { useEffect } from "react";
+import useEffectOnce from "@/shared/hooks/useEffectOnce";
+import { storage } from "@/shared/helpers";
+import i18n from "i18next";
 
-import AppStack from '@/shared/navigation/AppStack';
-import AuthStack from '@/shared/navigation/AuthStack';
-import { useAuth } from '@/shared/hooks/useAuth';
-import useFirebaseCloudMessaging from '@/shared/hooks/useFirebaseCloudMessaging';
-import useSplashScreen from '@/shared/hooks/useSplashScreen';
-import { UserProvider } from '@/shared/hooks/userContext';
-import { ToastProvider } from 'react-native-toast-notifications';
+import AppStack from "@/shared/navigation/AppStack";
+import AuthStack from "@/shared/navigation/AuthStack";
+import { useAuth } from "@/shared/hooks/useAuth";
+// import useFirebaseCloudMessaging from '@/shared/hooks/useFirebaseCloudMessaging';
+import {
+  NotificationListener,
+  requestUserPermission,
+} from "./src/shared/helpers/pushNotificationManager";
+import useSplashScreen from "@/shared/hooks/useSplashScreen"; 
+import { UserProvider } from "@/shared/hooks/userContext";
+import { ToastProvider } from "react-native-toast-notifications";
 
 function App(): JSX.Element {
-	// Push Notification hook
-	useFirebaseCloudMessaging();
-	// Splash Screen
-	useSplashScreen();
+  // Push Notification hook
+  // useFirebaseCloudMessaging();
+  // Splash Screen
+  useSplashScreen();
 
-	const { user } = useAuth();
+  const { user } = useAuth();
 
-	async function getTranslate() {
-		const ing = await storage.get('language');
-		console.log('ing', ing);
-		if (ing) {
-			await i18n.changeLanguage(ing);
-			return;
-		}
-		await i18n.changeLanguage('en');
-	}
-	useEffectOnce(() => {
-		getTranslate().catch();
-	}, []);
+  async function getTranslate() {
+    const ing = await storage.get("language");
+    console.log("ing", ing);
+    if (ing) {
+      await i18n.changeLanguage(ing);
+      return;
+    }
+    await i18n.changeLanguage("en");
+  }
 
-	return (
-		<ToastProvider>
-			<UserProvider>{user ? <AppStack /> : <AuthStack />}</UserProvider>
-		</ToastProvider>
-	);
+  useEffect(() => {
+    requestUserPermission();
+    NotificationListener();
+  }, []);
+
+  useEffectOnce(() => {
+    getTranslate().catch();
+  }, []);
+
+  return (
+    <ToastProvider>
+      <UserProvider>{user ? <AppStack /> : <AuthStack />}</UserProvider>
+    </ToastProvider>
+  );
 }
 
 export default App;
